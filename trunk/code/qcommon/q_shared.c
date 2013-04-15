ea571976aa652d32b87ddbf665f0cd653a8ccea4
nate.afk@gmail.com <nate.afk@gmail.com@e65d2741-a53d-b2dc-ae96-bb75fa5e4c4a>
2013-04-15 15:55:54 +0000

    Replaced va() with default rtcw one as current one completely breaks on deep nesting. Something like va(?, (arg ? va(?, arg2) : arg3)) will fail and produce either first or last output.. Additionally, this will solve drop messages in some cases.. (modding).

--- trunk/code/qcommon/q_shared.c
+++ trunk/code/qcommon/q_shared.c
@@ -1169,20 +1169,37 @@ va
 
 does a varargs printf into a temp buffer, so I don't need to have
 varargs versions of all text functions.
+
+Ridah, modified this into a circular list, to further prevent stepping on
+previous strings
 ============
 */
-char	* QDECL va( char *format, ... ) {
-	va_list		argptr;
-	static char string[2][32000]; // in case va is called by nested functions
-	static int	index = 0;
-	char		*buf;
+char    * QDECL va( char *format, ... ) {
+	va_list argptr;
+	#define MAX_VA_STRING   32000
+	static char temp_buffer[MAX_VA_STRING];
+	static char string[MAX_VA_STRING];      // in case va is called by nested functions
+	static int index = 0;
+	char    *buf;
+	int len;
 
-	buf = string[index & 1];
-	index++;
 
-	va_start (argptr, format);
-	Q_vsnprintf (buf, sizeof(*string), format, argptr);
-	va_end (argptr);
+	va_start( argptr, format );
+	vsprintf( temp_buffer, format,argptr );
+	va_end( argptr );
+
+	if ( ( len = strlen( temp_buffer ) ) >= MAX_VA_STRING ) {
+		Com_Error( ERR_DROP, "Attempted to overrun string in call to va()\n" );
+	}
+
+	if ( len + index >= MAX_VA_STRING - 1 ) {
+		index = 0;
+	}
+
+	buf = &string[index];
+	memcpy( buf, temp_buffer, len + 1 );
+
+	index += len + 1;
 
 	return buf;
 }
