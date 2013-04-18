a7e672df71c45d82e3d8806dfd4d887dafd7a02f
ZTurtleMan@gmail.com <ZTurtleMan@gmail.com@e65d2741-a53d-b2dc-ae96-bb75fa5e4c4a>
2013-04-18 17:01:47 +0000

    SP: Fix passing 64 bit pointers to VM native libs

--- trunk/SP/code/qcommon/qcommon.h
+++ trunk/SP/code/qcommon/qcommon.h
@@ -390,7 +390,7 @@ void	VM_Forced_Unload_Start(void);
 void	VM_Forced_Unload_Done(void);
 vm_t	*VM_Restart(vm_t *vm, qboolean unpure);
 
-intptr_t		QDECL VM_Call( vm_t *vm, int callNum, ... );
+intptr_t		QDECL VM_Call( vm_t *vm, intptr_t callNum, ... );
 
 void    VM_Debug( int level );
 
