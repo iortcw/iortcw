8f13d1f742a47002a5a80726ea585854c2f01500
M4N4T4RMS@gmail.com <M4N4T4RMS@gmail.com@e65d2741-a53d-b2dc-ae96-bb75fa5e4c4a>
2013-06-04 10:46:29 +0000

    SP: Revert unintended change to cl_cgame

--- trunk/SP/code/client/cl_cgame.c
+++ trunk/SP/code/client/cl_cgame.c
@@ -946,7 +946,7 @@ void CL_InitCGame( void ) {
 	const char          *info;
 	const char          *mapname;
 	int t1, t2;
-//	vmInterpret_t interpret;
+	vmInterpret_t interpret;
 
 	t1 = Sys_Milliseconds();
 
@@ -959,7 +959,6 @@ void CL_InitCGame( void ) {
 	Com_sprintf( cl.mapname, sizeof( cl.mapname ), "maps/%s.bsp", mapname );
 
 	// load the dll or bytecode
-#if 0
 	interpret = Cvar_VariableValue("vm_cgame");
 	if(cl_connectedToPureServer)
 	{
@@ -967,9 +966,8 @@ void CL_InitCGame( void ) {
 		if(interpret != VMI_COMPILED && interpret != VMI_BYTECODE)
 			interpret = VMI_COMPILED;
 	}
-#endif
-//	cgvm = VM_Create( "cgame", CL_CgameSystemCalls, interpret );
-	cgvm = VM_Create( "cgame", CL_CgameSystemCalls, Cvar_VariableValue( "vm_cgame" ) );
+	cgvm = VM_Create( "cgame", CL_CgameSystemCalls, interpret );
+//	cgvm = VM_Create( "cgame", CL_CgameSystemCalls, Cvar_VariableValue( "vm_cgame" ) );
 	if ( !cgvm ) {
 		Com_Error( ERR_DROP, "VM_Create on cgame failed" );
 	}
