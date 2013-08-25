b66c034d255871737d0645f3e2230c7024a85eac
M4N4T4RMS@gmail.com <M4N4T4RMS@gmail.com@e65d2741-a53d-b2dc-ae96-bb75fa5e4c4a>
2013-08-25 02:54:39 +0000

    SP: Add g_skipcutscenes cvar from bzzwolfsp

--- trunk/SP/code/game/g_main.c
+++ trunk/SP/code/game/g_main.c
@@ -54,6 +54,8 @@ extern int bg_pmove_gameskill_integer;
 
 vmCvar_t g_gametype;
 
+vmCvar_t g_skipcutscenes;
+
 // Rafael gameskill
 vmCvar_t g_gameskill;
 // done
@@ -167,7 +169,11 @@ cvarTable_t gameCvarTable[] = {
 	{ &g_soldierChargeTime, "g_soldierChargeTime", "20000", CVAR_SERVERINFO | CVAR_LATCH, 0, qfalse },
 // jpw
 
-
+#ifndef VCMODS_OPENGLES
+	{ &g_skipcutscenes, "g_skipcutscenes", "0", CVAR_ARCHIVE, 0, qtrue  },
+#else
+	{ &g_skipcutscenes, "g_skipcutscenes", "1", CVAR_ARCHIVE, 0, qtrue  },
+#endif
 	{ &g_playerStart, "g_playerStart", "0", CVAR_ROM, 0, qfalse  },
 
 	{ &g_maxclients, "sv_maxclients", "8", CVAR_SERVERINFO | CVAR_LATCH | CVAR_ARCHIVE, 0, qfalse  },
