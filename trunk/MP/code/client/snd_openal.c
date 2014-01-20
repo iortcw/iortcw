7c0450f8dc3266947aa0194a8283dd64cc373384
M4N4T4RMS@gmail.com <M4N4T4RMS@gmail.com@e65d2741-a53d-b2dc-ae96-bb75fa5e4c4a>
2014-01-20 22:08:17 +0000

    All: Change to full volume for AddRealLoopingSound (not used) in AL

--- trunk/MP/code/client/snd_openal.c
+++ trunk/MP/code/client/snd_openal.c
@@ -1508,7 +1508,7 @@ S_AL_AddRealLoopingSound
 */
 static void S_AL_AddRealLoopingSound(int entityNum, const vec3_t origin, const vec3_t velocity, const int range, sfxHandle_t sfx)
 {
-	S_AL_SrcLoop(SRCPRI_AMBIENT, sfx, origin, velocity, entityNum, 0);
+	S_AL_SrcLoop(SRCPRI_AMBIENT, sfx, origin, velocity, entityNum, 255);
 }
 
 /*
