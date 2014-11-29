c5285014cf4123e5f8eef1e4a15be7ce5db21a88
M4N4T4RMS@gmail.com <M4N4T4RMS@gmail.com@e65d2741-a53d-b2dc-ae96-bb75fa5e4c4a>
2014-11-29 10:24:38 +0000

    All: Rend2: Some culling fixes

--- MP/code/rend2/tr_init.c
+++ MP/code/rend2/tr_init.c
@@ -1020,6 +1020,8 @@ void GL_SetDefaultState( void ) {
 	//
 	glState.glStateBits = GLS_DEPTHTEST_DISABLE | GLS_DEPTHMASK_TRUE;
 	glState.storedGlState = 0;
+	glState.faceCulling = CT_TWO_SIDED;
+	glState.faceCullFront = CT_FRONT_SIDED;
 
 	glState.currentProgram = 0;
 	qglUseProgramObjectARB(0);
