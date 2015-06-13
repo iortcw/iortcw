41605e1dbf83b2c4493d91245117f601874efba8
MAN-AT-ARMS <M4N4T4RMS@gmail.com>
2015-06-13 06:51:46 -0400

    All: Don't crash if more than 128 modes are available

--- SP/code/sdl/sdl_glimp.c
+++ SP/code/sdl/sdl_glimp.c
@@ -157,7 +157,8 @@ static void GLimp_DetectAvailableModes(void)
 {
 	int i, j;
 	char buf[ MAX_STRING_CHARS ] = { 0 };
-	SDL_Rect modes[ 128 ];
+	size_t numSDLModes;
+	SDL_Rect *modes;
 	int numModes = 0;
 
 	int display = SDL_GetWindowDisplayIndex( SDL_window );
@@ -169,7 +170,14 @@ static void GLimp_DetectAvailableModes(void)
 		return;
 	}
 
-	for( i = 0; i < SDL_GetNumDisplayModes( display ); i++ )
+	numSDLModes = SDL_GetNumDisplayModes( display );
+	modes = SDL_calloc( numSDLModes, sizeof( SDL_Rect ) );
+	if ( !modes )
+	{
+		ri.Error( ERR_FATAL, "Out of memory" );
+	}
+
+	for( i = 0; i < numSDLModes; i++ )
 	{
 		SDL_DisplayMode mode;
 
@@ -179,6 +187,7 @@ static void GLimp_DetectAvailableModes(void)
 		if( !mode.w || !mode.h )
 		{
 			ri.Printf( PRINT_ALL, "Display supports any resolution\n" );
+			SDL_free( modes );
 			return;
 		}
 
@@ -220,6 +229,7 @@ static void GLimp_DetectAvailableModes(void)
 		ri.Printf( PRINT_ALL, "Available modes: '%s'\n", buf );
 		ri.Cvar_Set( "r_availableModes", buf );
 	}
+	SDL_free( modes );
 }
 
 /*
