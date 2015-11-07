a461a79fbec886e90fa1688ecead90f7d8febf5b
MAN-AT-ARMS <M4N4T4RMS@gmail.com>
2015-11-07 09:38:02 -0500

    All: Fix handling of language.cfg files (if present)

--- MP/code/qcommon/common.c
+++ MP/code/qcommon/common.c
@@ -2358,6 +2358,11 @@ For controlling environment variables
 void Com_ExecuteCfg(void)
 {
 	Cbuf_ExecuteText(EXEC_NOW, "exec default.cfg\n");
+	if ( FS_ReadFile( "language.cfg", NULL ) > 0 ) {
+		Cbuf_ExecuteText(EXEC_APPEND, "exec language.cfg\n");
+	} else if ( FS_ReadFile( "Language.cfg", NULL ) > 0 ) {
+		Cbuf_ExecuteText(EXEC_APPEND, "exec Language.cfg\n");
+	}
 	Cbuf_Execute(); // Always execute after exec to prevent text buffer overflowing
 
 	if(!Com_SafeMode())
