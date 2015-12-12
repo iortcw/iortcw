ab6b4571430f22a1e8b378d029687cfde473009e
MAN-AT-ARMS <M4N4T4RMS@gmail.com>
2015-12-12 11:24:30 -0500

    All: Fix the defines for OSX in minizip

--- SP/code/zlib-1.2.8/ioapi.c
+++ SP/code/zlib-1.2.8/ioapi.c
@@ -14,7 +14,7 @@
         #define _CRT_SECURE_NO_WARNINGS
 #endif
 
-#if defined(__MACOS_X__) || defined(IOAPI_NO_64)
+#if defined(MACOS_X) || defined(IOAPI_NO_64)
 // In darwin and perhaps other BSD variants off_t is a 64 bit value, hence no need for specific 64 bit functions
 #define FOPEN_FUNC(filename, mode) fopen(filename, mode)
 #define FTELLO_FUNC(stream) ftello(stream)
