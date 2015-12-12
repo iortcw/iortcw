7529771cf7f8c31d154ea1953571589903fd60f5
MAN-AT-ARMS <M4N4T4RMS@gmail.com>
2015-12-12 18:05:26 -0500

    All: Fix scaling for cinematics when using cg_fixedAspect

--- MP/code/ui/ui_main.c
+++ MP/code/ui/ui_main.c
@@ -1600,8 +1600,25 @@ static void UI_DrawClanCinematic( rectDef_t *rect, float scale, vec4_t color ) {
 			}
 			if ( uiInfo.teamList[i].cinematic >= 0 ) {
 				trap_CIN_RunCinematic( uiInfo.teamList[i].cinematic );
-				//FIXME:MAN-AT-ARMS Scale this
-				trap_CIN_SetExtents( uiInfo.teamList[i].cinematic, rect->x, rect->y, rect->w, rect->h );
+
+				if ( ui_fixedAspect.integer ) {
+					if ( DC->glconfig.vidWidth * 480.0 > DC->glconfig.vidHeight * 640.0 ) {
+						float scaledx = rect->x * ( 480.0 / 640.0 ) + ( DC->xBias / DC->xscaleStretch); 
+						float scaledw = rect->w * ( 480.0 / 640.0 );
+
+						trap_CIN_SetExtents( uiInfo.teamList[i].cinematic, scaledx, rect->y, scaledw, rect->h );
+					} else if ( DC->glconfig.vidWidth * 480.0 < DC->glconfig.vidHeight * 640.0 ) {
+						float scaledy = rect->y * ( 480.0 / 640.0 ) + ( DC->yBias / DC->yscaleStretch);
+						float scaledh = rect->h * ( 480.0 / 640.0 );
+
+						trap_CIN_SetExtents( uiInfo.teamList[i].cinematic, rect->x, scaledy, rect->w, scaledh );
+					} else {
+						trap_CIN_SetExtents( uiInfo.teamList[i].cinematic, rect->x, rect->y, rect->w, rect->h );
+					}
+				} else {
+					trap_CIN_SetExtents( uiInfo.teamList[i].cinematic, rect->x, rect->y, rect->w, rect->h );
+				}
+
 				trap_CIN_DrawCinematic( uiInfo.teamList[i].cinematic );
 			} else {
 				uiInfo.teamList[i].cinematic = -2;
@@ -1620,8 +1637,25 @@ static void UI_DrawPreviewCinematic( rectDef_t *rect, float scale, vec4_t color 
 		uiInfo.previewMovie = trap_CIN_PlayCinematic( va( "%s.roq", uiInfo.movieList[uiInfo.movieIndex] ), 0, 0, 0, 0, ( CIN_loop | CIN_silent ) );
 		if ( uiInfo.previewMovie >= 0 ) {
 			trap_CIN_RunCinematic( uiInfo.previewMovie );
-			//FIXME:MAN-AT-ARMS Scale this
-			trap_CIN_SetExtents( uiInfo.previewMovie, rect->x, rect->y, rect->w, rect->h );
+
+			if ( ui_fixedAspect.integer ) {
+				if ( DC->glconfig.vidWidth * 480.0 > DC->glconfig.vidHeight * 640.0 ) {
+					float scaledx = rect->x * ( 480.0 / 640.0 ) + ( DC->xBias / DC->xscaleStretch); 
+					float scaledw = rect->w * ( 480.0 / 640.0 );
+
+					trap_CIN_SetExtents( uiInfo.previewMovie, scaledx, rect->y, scaledw, rect->h );
+				} else if ( DC->glconfig.vidWidth * 480.0 < DC->glconfig.vidHeight * 640.0 ) {
+					float scaledy = rect->y * ( 480.0 / 640.0 ) + ( DC->yBias / DC->yscaleStretch);
+					float scaledh = rect->h * ( 480.0 / 640.0 );
+
+					trap_CIN_SetExtents( uiInfo.previewMovie, rect->x, scaledy, rect->w, scaledh );
+				} else {
+					trap_CIN_SetExtents( uiInfo.previewMovie, rect->x, rect->y, rect->w, rect->h );
+				}
+			} else {
+				trap_CIN_SetExtents( uiInfo.previewMovie, rect->x, rect->y, rect->w, rect->h );
+			}
+
 			trap_CIN_DrawCinematic( uiInfo.previewMovie );
 		} else {
 			uiInfo.previewMovie = -2;
@@ -1749,15 +1783,16 @@ static void UI_DrawMapCinematic( rectDef_t *rect, float scale, vec4_t color, qbo
 			trap_CIN_RunCinematic( uiInfo.mapList[map].cinematic );
 
 			if ( ui_fixedAspect.integer ) {
-				// HACK HACK HACK - FIXME: x,y scaling
 				if ( DC->glconfig.vidWidth * 480.0 > DC->glconfig.vidHeight * 640.0 ) {
+					float scaledx = rect->x * ( 480.0 / 640.0 ) + ( DC->xBias / DC->xscaleStretch); 
 					float scaledw = rect->w * ( 480.0 / 640.0 );
 
-					trap_CIN_SetExtents( uiInfo.mapList[map].cinematic, 103, rect->y, scaledw, rect->h );
+					trap_CIN_SetExtents( uiInfo.mapList[map].cinematic, scaledx, rect->y, scaledw, rect->h );
 				} else if ( DC->glconfig.vidWidth * 480.0 < DC->glconfig.vidHeight * 640.0 ) {
+					float scaledy = rect->y * ( 480.0 / 640.0 ) + ( DC->yBias / DC->yscaleStretch);
 					float scaledh = rect->h * ( 480.0 / 640.0 );
 
-					trap_CIN_SetExtents( uiInfo.mapList[map].cinematic, rect->x, 163, rect->w, scaledh );
+					trap_CIN_SetExtents( uiInfo.mapList[map].cinematic, rect->x, scaledy, rect->w, scaledh );
 				} else {
 					trap_CIN_SetExtents( uiInfo.mapList[map].cinematic, rect->x, rect->y, rect->w, rect->h );
 				}
@@ -1893,15 +1928,16 @@ static void UI_DrawNetMapCinematic( rectDef_t *rect, float scale, vec4_t color )
 		trap_CIN_RunCinematic( uiInfo.serverStatus.currentServerCinematic );
 
 		if ( ui_fixedAspect.integer ) {
-			// HACK HACK HACK - FIXME: x,y scaling
 			if ( DC->glconfig.vidWidth * 480.0 > DC->glconfig.vidHeight * 640.0 ) {
+				float scaledx = rect->x * ( 480.0 / 640.0 ) + ( DC->xBias / DC->xscaleStretch);
 				float scaledw = rect->w * ( 480.0 / 640.0 );
 
-				trap_CIN_SetExtents( uiInfo.serverStatus.currentServerCinematic, 469, rect->y, scaledw, rect->h );
+				trap_CIN_SetExtents( uiInfo.serverStatus.currentServerCinematic, scaledx, rect->y, scaledw, rect->h );
 			} else if ( DC->glconfig.vidWidth * 480.0 < DC->glconfig.vidHeight * 640.0 ) {
+				float scaledy = rect->y * ( 480.0 / 640.0 ) + ( DC->yBias / DC->yscaleStretch);
 				float scaledh = rect->h * ( 480.0 / 640.0 );
 
-				trap_CIN_SetExtents( uiInfo.serverStatus.currentServerCinematic, rect->x, 150, rect->w, scaledh );
+				trap_CIN_SetExtents( uiInfo.serverStatus.currentServerCinematic, rect->x, scaledy, rect->w, scaledh );
 			} else {
 				trap_CIN_SetExtents( uiInfo.serverStatus.currentServerCinematic, rect->x, rect->y, rect->w, rect->h );
 			}
@@ -6771,20 +6807,23 @@ static void UI_StopCinematic( int handle ) {
 
 static void UI_DrawCinematic( int handle, float x, float y, float w, float h ) {
 	if ( ui_fixedAspect.integer ) {
-		// HACK HACK HACK - FIXME: x,y scaling
 		if ( DC->glconfig.vidWidth * 480.0 > DC->glconfig.vidHeight * 640.0 ) {
+			float scaledx = x * ( 480.0 / 640.0 ) + ( DC->xBias / DC->xscaleStretch);
 			float scaledw = w * ( 480.0 / 640.0 );
 
-			trap_CIN_SetExtents( handle, 202, y, scaledw, h );
+			trap_CIN_SetExtents( handle, scaledx, y, scaledw, h );
 		} else if ( DC->glconfig.vidWidth * 480.0 < DC->glconfig.vidHeight * 640.0 ) {
+			float scaledy = y * ( 480.0 / 640.0 ) + ( DC->yBias / DC->yscaleStretch);
 			float scaledh = h * ( 480.0 / 640.0 );
 
-			trap_CIN_SetExtents( handle, x, 190, w, scaledh );
+			trap_CIN_SetExtents( handle, x, scaledy, w, scaledh );
 		} else {
 			trap_CIN_SetExtents( handle, x, y, w, h );
 		}
+	} else {
+		trap_CIN_SetExtents( handle, x, y, w, h );
 	}
-	//trap_CIN_SetExtents( handle, x, y, w, h );
+
 	trap_CIN_DrawCinematic( handle );
 }
 
