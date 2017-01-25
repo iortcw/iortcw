46fef7ff8c4133fca798ab9dc2aa590997700fa7
MAN-AT-ARMS <M4N4T4RMS@gmail.com>
2017-01-25 17:30:46 -0500

    All: Rephrase SSE availability message

--- SP/code/qcommon/common.c
+++ SP/code/qcommon/common.c
@@ -2227,7 +2227,7 @@ static void Com_DetectSSE(void)
 #endif
 		Q_VMftol = qvmftolsse;
 
-		Com_Printf("Have SSE support\n");
+		Com_Printf("SSE instruction set enabled\n");
 #if !idx64
 	}
 	else
@@ -2236,7 +2236,7 @@ static void Com_DetectSSE(void)
 		Q_VMftol = qvmftolx87;
 		Q_SnapVector = qsnapvectorx87;
 
-		Com_Printf("No SSE support on this machine\n");
+		Com_Printf("SSE instruction set not available\n");
 	}
 #endif
 }
