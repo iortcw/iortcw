43ed9ff3063c512a5fe9bfd15bc7b01fd7a3c9c2
MAN-AT-ARMS <M4N4T4RMS@gmail.com>
2017-03-14 22:17:56 -0400

    All: Fix extension check for Com_WriteConfig_f

--- SP/code/qcommon/common.c
+++ SP/code/qcommon/common.c
@@ -2601,14 +2601,15 @@ void Com_WriteConfig_f( void ) {
 		return;
 	}
 
+	Q_strncpyz( filename, Cmd_Argv( 1 ), sizeof( filename ) );
+	COM_DefaultExtension( filename, sizeof( filename ), ".cfg" );
+
 	if (!COM_CompareExtension(filename, ".cfg"))
 	{
 		Com_Printf("Com_WriteConfig_f: Only the \".cfg\" extension is supported by this command!\n");
 		return;
 	}
 
-	Q_strncpyz( filename, Cmd_Argv( 1 ), sizeof( filename ) );
-	COM_DefaultExtension( filename, sizeof( filename ), ".cfg" );
 	Com_Printf( "Writing %s.\n", filename );
 	Com_WriteConfigToFile( filename );
 }
