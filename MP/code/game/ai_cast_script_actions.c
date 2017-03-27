iortcw/iortcw

commit d6ee8fc999e4094eac4a66d0f357441fc55316bb

All: Source alignment and cleanup - Part3

MP/code/game/ai_cast_script_actions.c
@@ -179,7 +179,7 @@ qboolean AICast_ScriptAction_GotoMarker( cast_state_t *cs, char *params ) {
 	}
 
 	// find the ai_marker with the given "targetname"
-	// TTimo gcc: suggest parentheses around assignment used as truth value
+
 	while ( ( ent = G_Find( ent, FOFS( classname ), "ai_marker" ) ) )
 	{
 		if ( ent->targetname && !Q_strcasecmp( ent->targetname, token ) ) {
@@ -489,7 +489,7 @@ qboolean AICast_ScriptAction_Wait( cast_state_t *cs, char *params ) {
 					&&  ( cs->bs->enemy >= 0 ) ) {
 
 				// if we can attack them, or they can't attack us, stay here
-				// TTimo gcc: suggest parentheses around && within ||
+
 				if (    AICast_CheckAttack( cs, cs->bs->enemy, qfalse )
 						||  (   !AICast_EntityVisible( AICast_GetCastState( cs->bs->enemy ), cs->entityNum, qfalse )
 								&&  !AICast_CheckAttack( AICast_GetCastState( cs->bs->enemy ), cs->entityNum, qfalse ) ) ) {
@@ -887,11 +887,8 @@ qboolean AICast_ScriptAction_SetAmmo( cast_state_t *cs, char *params ) {
 //----(SA)	end
 
 	} else {
-
 //		G_Printf( "--SCRIPTER WARNING-- AI Scripting: setammo: unknown ammo \"%s\"", params );
 		return qfalse;  // (SA) temp as scripts transition to new names
-//		G_Error( "AI Scripting: setammo: unknown ammo \"%s\"", params );
-
 	}
 
 	return qtrue;
@@ -954,7 +951,6 @@ qboolean AICast_ScriptAction_SetClip( cast_state_t *cs, char *params ) {
 	} else {
 //		G_Printf( "--SCRIPTER WARNING-- AI Scripting: setclip: unknown weapon \"%s\"", params );
 		return qfalse;  // (SA) temp as scripts transition to new names
-//		G_Error( "AI Scripting: setclip: unknown weapon \"%s\"", params );
 	}
 
 	return qtrue;
@@ -998,11 +994,10 @@ qboolean AICast_ScriptAction_SelectWeapon( cast_state_t *cs, char *params ) {
 
 		if ( !cs->aiCharacter ) {  // only do this for player
 			g_entities[cs->entityNum].client->ps.weaponTime = 500;  // (SA) HACK: FIXME: TODO: delay to catch initial weapon reload
-
 		}
 	} else {
 //		G_Printf( "--SCRIPTER WARNING-- AI Scripting: selectweapon: unknown weapon \"%s\"", params );
-		return qfalse;  // (SA) temp as scripts transition to new names
+		return qfalse;	// (SA) temp as scripts transition to new names
 //		G_Error( "AI Scripting: selectweapon: unknown weapon \"%s\"", params );
 	}
 
@@ -1024,8 +1019,6 @@ AICast_ScriptAction_GiveArmor
 */
 qboolean AICast_ScriptAction_GiveArmor( cast_state_t *cs, char *params ) {
 	int i;
-	// TTimo unused
-//	gentity_t	*ent=&g_entities[cs->entityNum];
 	gitem_t     *item = 0;
 
 	for ( i = 1; bg_itemlist[i].classname; i++ ) {
@@ -1203,8 +1196,6 @@ AICast_ScriptAction_GiveInventory
 */
 qboolean AICast_ScriptAction_GiveInventory( cast_state_t *cs, char *params ) {
 	int i;
-	// TTimo unused
-//	gentity_t	*ent=&g_entities[cs->entityNum];
 	gitem_t     *item = 0;
 
 	for ( i = 1; bg_itemlist[i].classname; i++ ) {
@@ -1269,6 +1260,8 @@ qboolean AICast_ScriptAction_Movetype( cast_state_t *cs, char *params ) {
 
 	return qtrue;
 }
+
+
 /*
 =================
 AICast_ScriptAction_AlertEntity
@@ -1321,7 +1314,7 @@ AICast_ScriptAction_SaveGame
 =================
 */
 qboolean AICast_ScriptAction_SaveGame( cast_state_t *cs, char *params ) {
-	char *pString;
+	char *pString; //, *saveName;
 	pString = params;
 
 	if ( cs->bs ) {
@@ -1330,10 +1323,11 @@ qboolean AICast_ScriptAction_SaveGame( cast_state_t *cs, char *params ) {
 
 //----(SA)	check for parameter
 	COM_ParseExt( &pString, qfalse );
-//	if (!saveName[0])
+//	if ( !saveName[0] ) {
 //		G_SaveGame( NULL );	// save the default "current" savegame
-//	else
+//	} else {
 //		G_SaveGame( saveName );
+//	}
 //----(SA)	end
 
 	return qtrue;
@@ -1960,9 +1954,9 @@ qboolean AICast_ScriptAction_ChangeLevel( cast_state_t *cs, char *params ) {
 		trap_Cvar_Set( "g_missionStats", cmd );
 	}
 
-	// save persistant data if required
+	// save persistent data if required
 	newstr = va( "%s", params );
-	pch = strstr( newstr, " persistant" );
+	pch = strstr( newstr, " persistent" );
 	if ( pch ) {
 		pch = strstr( newstr, " " );
 		*pch = '\0';
@@ -2290,8 +2284,6 @@ qboolean AICast_ScriptAction_StartCam( cast_state_t *cs, char *params ) {
 qboolean AICast_ScriptAction_StartCamBlack( cast_state_t *cs, char *params ) {
 	return ScriptStartCam( cs, params, qtrue );
 }
-
-
 //----(SA)	end
 
 /*

SP/code/game/ai_cast_script_actions.c
@@ -189,7 +189,7 @@ qboolean AICast_ScriptAction_GotoMarker( cast_state_t *cs, char *params ) {
 	}
 
 	// find the ai_marker with the given "targetname"
-	// TTimo: gcc: suggest () around assignment used as truth value
+
 	while ( ( ent = G_Find( ent, FOFS( classname ), "ai_marker" ) ) )
 	{
 		if ( ent->targetname && !Q_strcasecmp( ent->targetname, token ) ) {
@@ -537,9 +537,9 @@ qboolean AICast_ScriptAction_Wait( cast_state_t *cs, char *params ) {
 	}
 
 	// default to no moverange
-	//if (moverange == -999) {
-	//	moverange = 200;
-	//}
+//	if ( moverange == -999 ) {
+//		moverange = 200;
+//	}
 
 	if ( moverange != 0 ) {       // default to 200 if no range given
 		if ( moverange > 0 ) {
@@ -549,7 +549,7 @@ qboolean AICast_ScriptAction_Wait( cast_state_t *cs, char *params ) {
 					&&  ( cs->enemyNum >= 0 ) ) {
 
 				// if we can attack them, or they can't attack us, stay here
-				// TTimo: gcc: warning: suggest parentheses around && within ||
+
 				if (    AICast_CheckAttack( cs, cs->enemyNum, qfalse )
 						||  (   !AICast_EntityVisible( AICast_GetCastState( cs->enemyNum ), cs->entityNum, qfalse )
 								&&  !AICast_CheckAttack( AICast_GetCastState( cs->enemyNum ), cs->entityNum, qfalse ) ) ) {
@@ -1064,12 +1064,10 @@ qboolean AICast_ScriptAction_SetAmmo( cast_state_t *cs, char *params ) {
 		}
 
 	} else {
-
 		if ( g_cheats.integer ) {
 			G_Printf( "--SCRIPTER WARNING-- AI Scripting: setammo: unknown ammo \"%s\"", params );
 		}
 		return qfalse;  // (SA) temp as scripts transition to new names
-
 	}
 
 	return qtrue;
@@ -1132,7 +1130,6 @@ qboolean AICast_ScriptAction_SetClip( cast_state_t *cs, char *params ) {
 	} else {
 //		G_Printf( "--SCRIPTER WARNING-- AI Scripting: setclip: unknown weapon \"%s\"", params );
 		return qfalse;  // (SA) temp as scripts transition to new names
-//		G_Error( "AI Scripting: setclip: unknown weapon \"%s\"", params );
 	}
 
 	return qtrue;
@@ -1214,14 +1211,8 @@ qboolean AICast_ScriptAction_SelectWeapon( cast_state_t *cs, char *params ) {
 		g_entities[cs->entityNum].client->ps.weaponstate = WEAPON_READY;
 
 		if ( !cs->aiCharacter ) {  // only do this for player
-			//vmCvar_t cvar;
-			//
 			g_entities[cs->entityNum].client->ps.weaponTime = 750;  // (SA) HACK: FIXME: TODO: delay to catch initial weapon reload
-			// tell it which weapon to use after spawning in
-			//trap_Cvar_Register( &cvar, "cg_loadWeaponSelect", "0", CVAR_ROM );
-			//trap_Cvar_Set( "cg_loadWeaponSelect", va("%i", g_entities[cs->entityNum].client->ps.weapon ) );
 		}
-
 	} else {
 //		G_Printf( "--SCRIPTER WARNING-- AI Scripting: selectweapon: unknown weapon \"%s\"", params );
 //		return qfalse;	// (SA) temp as scripts transition to new names
@@ -1387,7 +1378,6 @@ qboolean AICast_ScriptAction_TakeWeapon( cast_state_t *cs, char *params ) {
 		cs->weaponNum = WP_NONE;
 
 	} else {
-
 
 		for ( i = 1; bg_itemlist[i].classname; i++ )
 		{
@@ -1589,7 +1579,7 @@ qboolean AICast_ScriptAction_SaveGame( cast_state_t *cs, char *params ) {
 //----(SA)	check for parameter
 	saveName = COM_ParseExt( &pString, qfalse );
 	if ( !saveName[0] ) {
-		G_SaveGame( NULL ); // save the default "current" savegame
+		G_SaveGame( NULL );	// save the default "current" savegame
 	} else {
 		G_SaveGame( saveName );
 	}
@@ -1609,7 +1599,8 @@ qboolean AICast_ScriptAction_FireAtTarget( cast_state_t *cs, char *params ) {
 	gentity_t   *ent;
 	vec3_t vec, org, src;
 	char *pString, *token;
-	int i, diff;
+	float diff;
+	int i;
 
 	pString = params;
 
@@ -2650,6 +2641,7 @@ qboolean AICast_ScriptAction_Zoom( cast_state_t *cs, char *params ) {
 
 }
 
+//----(SA)	added
 /*
 ===================
 AICast_ScriptAction_StartCam
@@ -2690,8 +2682,6 @@ qboolean AICast_ScriptAction_StartCamBlack( cast_state_t *cs, char *params ) {
 	return ScriptStartCam( cs, params, qtrue );
 }
 
-
-//----(SA)	added
 qboolean AICast_ScriptAction_StopCamBlack( cast_state_t *cs, char *params ) {
 	trap_SendServerCommand( cs->entityNum, "stopCamblack" );
 	return qtrue;
@@ -2701,10 +2691,7 @@ qboolean AICast_ScriptAction_StopCam( cast_state_t *cs, char *params ) {
 	trap_SendServerCommand( cs->entityNum, "stopCam" );
 	return qtrue;
 }
-//----(SA)	end
 
-
-//----(SA)	added
 qboolean AICast_ScriptAction_Cigarette( cast_state_t *cs, char *params ) {
 	char    *pString, *token;
 
@@ -2852,10 +2839,6 @@ qboolean AICast_ScriptAction_Cvar( cast_state_t *cs, char *params ) {
 	trap_Cvar_Set( cvarName, token );
 	return qtrue;
 }
-
-
-
-//----(SA)	added some music interface
 
 /*
 ==================

MP/code/game/ai_cast.c
@@ -373,6 +373,7 @@ gentity_t *AICast_CreateCharacter( gentity_t *ent, float *attributes, cast_weapo
 	//
 	cs = AICast_GetCastState( newent->s.number );
 	//
+	cs->aiCharacter = ent->aiCharacter;
 	// setup the attributes
 	memcpy( cs->attributes, attributes, sizeof( cs->attributes ) );
 	ppStr = &ent->aiAttributes;
@@ -392,7 +393,6 @@ gentity_t *AICast_CreateCharacter( gentity_t *ent, float *attributes, cast_weapo
 	cs->bs->enemy = -1;
 	cs->leaderNum = -1;
 	cs->castScriptStatus.scriptGotoEnt = -1;
-	cs->aiCharacter = ent->aiCharacter;
 	//
 	newent->aiName = ent->aiName;
 	newent->aiTeam = ent->aiTeam;
@@ -730,12 +730,12 @@ void AICast_CheckLoadGame( void ) {
 
 		if ( ready ) {
 			trap_Cvar_Set( "savegame_loading", "0" ); // in-case it aborts
+			saveGamePending = qfalse;
 //			G_LoadGame( NULL );		// always load the "current" savegame
 			trap_Cvar_Set( "cg_norender", "0" );
-			saveGamePending = qfalse;
 
 			// wait for the clients to return from faded screen
-			// trap_SetConfigstring( CS_SCREENFADE, va("0 %i 1500", level.time + 500) );
+//			trap_SetConfigstring( CS_SCREENFADE, va("0 %i 1500", level.time + 500) );
 			trap_SetConfigstring( CS_SCREENFADE, va( "0 %i 750", level.time + 500 ) );
 			level.reloadPauseTime = level.time + 1100;
 
@@ -755,12 +755,12 @@ void AICast_CheckLoadGame( void ) {
 		// not loading a game, we must be in a new level, so look for some persistant data to read in, then save the game
 		if ( ready ) {
 //			G_LoadPersistant();		// make sure we save the game after we have brought across the items
-//			G_SaveGame( NULL );
+
 			trap_Cvar_Set( "cg_norender", "0" );
 			saveGamePending = qfalse;
 
 			// wait for the clients to return from faded screen
-			// trap_SetConfigstring( CS_SCREENFADE, va("0 %i 1500", level.time + 500) );
+//			trap_SetConfigstring( CS_SCREENFADE, va( "0 %i 1500", level.time + 500 ) );
 			trap_SetConfigstring( CS_SCREENFADE, va( "0 %i 750", level.time + 500 ) );
 			level.reloadPauseTime = level.time + 1100;
 

MP/code/game/ai_cast.h
@@ -194,10 +194,10 @@ extern AICharacterDefaults_t aiDefaults[NUM_CHARACTERS];
 //
 // structure defines
 //
-#define AIVIS_ENEMY         1
-#define AIVIS_INSPECTED     2       // we have inspected them once already
-#define AIVIS_INSPECT       4       // we should inspect them when we get a chance
-#define AIVIS_PROCESS_SIGHTING  8   // so we know if we have or haven't processed the sighting since they were last seen
+#define AIVIS_ENEMY					1
+#define AIVIS_INSPECTED				2		// we have inspected them once already
+#define AIVIS_INSPECT				4		// we should inspect them when we get a chance
+#define AIVIS_PROCESS_SIGHTING		8		// so we know if we have or haven't processed the sighting since they were last seen
 //
 // share range
 #define AIVIS_SHARE_RANGE   170     // if we are within this range of a friendly, share their vis info
@@ -532,7 +532,7 @@ typedef struct cast_state_s
 
 } cast_state_t;
 //
-#define CSFOFS( x ) ( (int)&( ( (cast_state_t *)0 )->x ) )
+#define CSFOFS( x ) ( (size_t)&( ( (cast_state_t *)0 )->x ) )
 //
 typedef struct aicast_predictmove_s
 {

MP/code/game/ai_cast_debug.c
@@ -114,7 +114,6 @@ AICast_DBG_RouteTable_f
 */
 void AICast_DBG_RouteTable_f( vec3_t org, char *param ) {
 	static int srcarea = 0, dstarea = 0;
-	// TTimo unused
 //	extern botlib_export_t botlib;
 
 	if ( !param || strlen( param ) < 1 ) {
@@ -150,7 +149,7 @@ AICast_DBG_Spawn_f
 ===============
 */
 void AICast_DBG_Spawn_f( gclient_t *client, char *cmd ) {
-	extern qboolean G_CallSpawn( gentity_t * ent );
+	extern qboolean G_CallSpawn( gentity_t *ent );
 	gentity_t   *ent;
 	vec3_t dir;
 

MP/code/game/ai_cast_events.c
@@ -213,19 +213,6 @@ void AICast_Die( gentity_t *self, gentity_t *inflictor, gentity_t *attacker, int
 				// RF, changed this so Zombies always gib now
 				GibEntity( self, killer );
 				nogib = qfalse;
-/*
-				// Zombie has special exploding cloud effect
-				if (attacker != inflictor || attacker->s.weapon == WP_VENOM)
-				{
-					GibEntity( self, killer );
-					nogib = qfalse;
-				} else {
-					// Zombie will decompose upon dying
-					self->client->ps.eFlags |= EF_MONSTER_EFFECT2;
-					self->s.effect2Time = level.time+200;
-					self->health = -1;
-				}
-*/
 				self->takedamage = qfalse;
 				self->r.contents = 0;
 				cs->secondDeadTime = 2;
@@ -286,24 +273,6 @@ void AICast_Die( gentity_t *self, gentity_t *inflictor, gentity_t *attacker, int
 				// RF, changed this so Zombies always gib now
 				GibEntity( self, killer );
 				nogib = qfalse;
-/*
-				// Zombie has special exploding cloud effect
-				if (attacker != inflictor || attacker->s.weapon == WP_VENOM)
-				{
-					GibEntity( self, killer );
-					nogib = qfalse;
-					self->takedamage = qfalse;
-					self->r.contents = 0;
-					cs->secondDeadTime = 2;
-				} else {
-					self->client->ps.eFlags |= EF_MONSTER_EFFECT2;
-					self->s.effect2Time = level.time+200;
-					self->takedamage = qfalse;
-					self->r.contents = 0;
-					self->health = -1;
-					cs->secondDeadTime = 2;
-				}
-*/
 			} else if ( !( contents & CONTENTS_NODROP ) ) {
 				body_die( self, inflictor, attacker, damage, meansOfDeath );
 				//GibEntity( self, killer );
@@ -430,7 +399,6 @@ void AICast_AIDoor_Touch( gentity_t *ent, gentity_t *aidoor_trigger, gentity_t *
 		}
 	}
 
-	// TTimo: assignment used as truth value
 	for ( trav = NULL; ( trav = G_Find( trav, FOFS( target ), aidoor_trigger->targetname ) ); ) {
 		// make sure the marker is vacant
 		trap_Trace( &tr, trav->r.currentOrigin, ent->r.mins, ent->r.maxs, trav->r.currentOrigin, ent->s.number, ent->clipmask );

MP/code/game/ai_cast_fight.c
@@ -1265,7 +1265,7 @@ AICast_CombatMove
 bot_moveresult_t AICast_CombatMove( cast_state_t *cs, int tfl ) {
 	bot_state_t *bs;
 	float dist;
-	vec3_t forward; //, up = {0, 0, 1};
+	vec3_t forward;
 	bot_moveresult_t moveresult;
 	bot_goal_t goal;
 
@@ -1273,7 +1273,6 @@ bot_moveresult_t AICast_CombatMove( cast_state_t *cs, int tfl ) {
 
 	//get the enemy entity info
 	memset( &moveresult, 0, sizeof( bot_moveresult_t ) );
-
 	//initialize the movement state
 	BotSetupForMovement( bs );
 	//direction towards the enemy
@@ -1468,30 +1467,6 @@ qboolean AICast_AimAtEnemy( cast_state_t *cs ) {
 			bestorigin[2] += 16;
 		}
 	}
-	//
-	// adjust accuracy with distance if upclose, so we don't start firing the wrong direction
-/*
-	scale = 1;
-	switch (cs->bs->weaponnum) {
-	// these weapons don't do random offsetting
-	case WP_FLAMETHROWER:
-		break;
-	default:
-		{
-			float scale;
-
-			if (dist < 256)
-				scale *= (dist / 256);
-			else
-				scale *= 1.0;
-
-			bestorigin[0] += scale * 96 * sin((float)level.time/(200.0 + (40.0*((cs->entityNum+3)%4)))) * (1 - aim_accuracy);
-			bestorigin[1] += scale * 96 * cos((float)level.time/(220.0 + (36.0*((cs->entityNum+1)%5)))) * (1 - aim_accuracy);
-			bestorigin[2] += scale * 48 * sin((float)level.time/(210.0 + (32.0*((cs->entityNum+2)%6)))) * (1 - aim_accuracy);
-		}
-		break;
-	}
-*/
 	// if the enemy is moving, they are harder to hit
 	if ( dist > 256 ) {
 		VectorMA( bestorigin, ( 0.3 + 0.7 * ( 1 - aim_accuracy ) ) * 0.4 * sin( (float)level.time / ( 500.0 + ( 100.0 * ( ( cs->entityNum + 3 ) % 4 ) ) ) ), g_entities[bs->enemy].client->ps.velocity, bestorigin );
@@ -1515,7 +1490,7 @@ qboolean AICast_CanMoveWhileFiringWeapon( int weaponnum ) {
 	case WP_GARAND:
 	case WP_SNIPERRIFLE:    //----(SA)	added
 	case WP_SNOOPERSCOPE:   //----(SA)	added
-	case WP_FG42SCOPE:      //----(SA)	added
+	case WP_FG42SCOPE:		//----(SA)	added
 	case WP_PANZERFAUST:
 	case WP_ROCKET_LAUNCHER:
 		return qfalse;
@@ -1732,13 +1707,6 @@ void AICast_RecordWeaponFire( gentity_t *ent ) {
 	range = AICast_GetWeaponSoundRange( cs->lastWeaponFiredWeaponNum );
 
 	AICast_AudibleEvent( cs->entityNum, cs->lastWeaponFiredPos, range );
-	//AICast_SightSoundEvent( cs, range );
-	/*
-	if (!cs->sightSoundTime || cs->sightSoundRange < range) {
-		cs->sightSoundRange = range;
-		cs->sightSoundTime = level.time;
-	}
-	*/
 
 	if ( cs->bs ) {   // real player's don't need to play AI sounds
 		AIChar_AttackSound( cs );
@@ -1926,9 +1894,9 @@ int AICast_SafeMissileFire( gentity_t *ent, int duration, int enemyNum, vec3_t e
 	rval = ( Distance( org, enemyPos ) < ent->splashRadius ) && AICast_VisibleFromPos( org, ent->s.number, enemyPos, enemyNum, qfalse );
 	if ( rval ) {
 		// don't hurt ourselves
-		// disabled, don't worry about us, we can get out the way in time (we hope!)
-		//if (Distance( org, g_entities[selfNum].r.currentOrigin ) < ent->splashRadius*1.5)
-		//	return -1;
+//		if ( Distance( org, g_entities[selfNum].r.currentOrigin ) < ent->splashRadius * 1.5 ) {
+//			return -1;
+//		}
 		// make sure we don't injure a friendly
 		for ( trav = g_entities; trav < g_entities + g_maxclients.integer; trav++ ) {
 			if ( !trav->inuse ) {
@@ -2181,8 +2149,6 @@ void AICast_AudibleEvent( int srcnum, vec3_t pos, float range ) {
 		if ( !cs->bs ) {
 			continue;
 		}
-		//if (cs->aiState >= AISTATE_COMBAT)
-		//	continue;
 		if ( ent == sent ) {
 			continue;
 		}

commit a2af6ca3054374f5cedc36e6efd5c6712e70b418

SP: Fix regression in AICast_ScriptAction_FireAtTarget
Introduced in d6ee8fc
Unused in MP but merged from SP

MP/code/game/ai_cast_script_actions.c
@@ -1344,8 +1344,7 @@ qboolean AICast_ScriptAction_FireAtTarget( cast_state_t *cs, char *params ) {
 	gentity_t   *ent;
 	vec3_t vec, org, src;
 	char *pString, *token;
-	float diff;
-	int i;
+	int i, diff;
 
 	pString = params;
 

MP/code/ui/ui_atoms.c
@@ -61,7 +61,6 @@ void QDECL Com_DPrintf( const char *fmt, ... ) {
 }
 // jpw
 
-
 void QDECL Com_Error( int level, const char *error, ... ) {
 	va_list argptr;
 	char text[1024];

SP/code/game/ai_cast_script_actions.c
@@ -1599,8 +1599,7 @@ qboolean AICast_ScriptAction_FireAtTarget( cast_state_t *cs, char *params ) {
 	gentity_t   *ent;
 	vec3_t vec, org, src;
 	char *pString, *token;
-	float diff;
-	int i;
+	int i, diff;
 
 	pString = params;
 