4b2a1f9ad2ec9cea641ce8858fc38716742cf69e
MAN-AT-ARMS <M4N4T4RMS@gmail.com>
2017-06-08 11:26:46 -0400

    All: Bump version to 1.51b

--- SP/code/qcommon/q_shared.h
+++ SP/code/qcommon/q_shared.h
@@ -85,7 +85,7 @@ If you have questions concerning this license or the applicable additional terms
 #define LEGACY_HEARTBEAT_FOR_MASTER	"Wolfenstein-1"
 
 #ifndef PRODUCT_VERSION
-  #define PRODUCT_VERSION "1.51-SP"
+  #define PRODUCT_VERSION "1.51b-SP"
 #endif
 
 #ifndef PRODUCT_DATE
