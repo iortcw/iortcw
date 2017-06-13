7a3a0a868239c64a657a128b76fc790ce22c6a2b
MAN-AT-ARMS <M4N4T4RMS@gmail.com>
2017-06-13 09:53:43 -0400

    All: Don't build score info for bots, they don't parse it

--- SP/code/game/g_cmds.c
+++ SP/code/game/g_cmds.c
@@ -43,6 +43,11 @@ void DeathmatchScoreboardMessage( gentity_t *ent ) {
 	int numSorted;
 	int scoreFlags;
 
+	// don't send scores to bots, they don't parse it
+	if ( ent->r.svFlags & SVF_BOT ) {
+		return;
+	}
+
 	// send the latest information on all clients
 	string[0] = 0;
 	stringlength = 0;
