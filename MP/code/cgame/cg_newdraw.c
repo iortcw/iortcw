a73b906d97e2364e20e7b08f97d35b3e2f4fec0c
MAN-AT-ARMS <M4N4T4RMS@gmail.com>
2017-07-18 09:14:24 -0400

    All: Check for unlimited time power up using INT_MAX

--- MP/code/cgame/cg_newdraw.c
+++ MP/code/cgame/cg_newdraw.c
@@ -1421,10 +1421,15 @@ static void CG_DrawAreaPowerUp( rectDef_t *rect, int align, float spacing, float
 		if ( !ps->powerups[ i ] ) {
 			continue;
 		}
-		t = ps->powerups[ i ] - cg.time;
-		// ZOID--don't draw if the power up has unlimited time (999 seconds)
+
+		// ZOID--don't draw if the power up has unlimited time
 		// This is true of the CTF flags
-		if ( t <= 0 || t >= 999000 ) {
+		if ( ps->powerups[ i ] == INT_MAX ) {
+			continue;
+		}
+
+		t = ps->powerups[ i ] - cg.time;
+		if ( t <= 0 ) {
 			continue;
 		}
 
--- SP/code/cgame/cg_newdraw.c
+++ SP/code/cgame/cg_newdraw.c
@@ -1358,10 +1358,15 @@ static void CG_DrawAreaPowerUp( rectDef_t *rect, int align, float spacing, int f
 		if ( !ps->powerups[ i ] ) {
 			continue;
 		}
-		t = ps->powerups[ i ] - cg.time;
-		// ZOID--don't draw if the power up has unlimited time (999 seconds)
+
+		// ZOID--don't draw if the power up has unlimited time
 		// This is true of the CTF flags
-		if ( t <= 0 || t >= 999000 ) {
+		if ( ps->powerups[ i ] == INT_MAX ) {
+			continue;
+		}
+
+		t = ps->powerups[ i ] - cg.time;
+		if ( t <= 0 ) {
 			continue;
 		}
 
