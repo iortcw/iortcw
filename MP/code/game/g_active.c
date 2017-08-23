8d47e6714f421b89bc5246e2b4b4ba311b7b26cd
MAN-AT-ARMS <M4N4T4RMS@gmail.com>
2017-08-22 23:16:01 -0400

    All: Fix crash when pmove_msec is 0

--- MP/code/game/g_active.c
+++ MP/code/game/g_active.c
@@ -854,8 +854,10 @@ void ClientThink_real( gentity_t *ent ) {
 
 	if ( pmove_msec.integer < 8 ) {
 		trap_Cvar_Set( "pmove_msec", "8" );
-	} else if ( pmove_msec.integer > 33 )     {
+		trap_Cvar_Update( &pmove_msec );
+	} else if ( pmove_msec.integer > 33 ) {
 		trap_Cvar_Set( "pmove_msec", "33" );
+		trap_Cvar_Update( &pmove_msec );
 	}
 
 	if ( pmove_fixed.integer || client->pers.pmoveFixed ) {
