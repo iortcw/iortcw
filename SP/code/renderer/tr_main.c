aab94bf44cd598efd13641266c4ab5031812dfa7
MAN-AT-ARMS <M4N4T4RMS@gmail.com>
2017-10-11 17:07:19 -0400

    All: Only draw cm_patch/bot debug polygons in world scenes

--- SP/code/renderer/tr_main.c
+++ SP/code/renderer/tr_main.c
@@ -1704,6 +1704,9 @@ Visualization aid for movement clipping debugging
 ====================
 */
 void R_DebugGraphics( void ) {
+	if ( tr.refdef.rdflags & RDF_NOWORLDMODEL ) {
+		return;
+	}
 	if ( !r_debugSurface->integer ) {
 		return;
 	}
