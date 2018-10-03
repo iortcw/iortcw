4ddbf8b98f22bcb35e0909be80e2094890ccaed3
MAN-AT-ARMS <M4N4T4RMS@gmail.com>
2018-10-03 10:26:23 -0400

    All: Allow binds to use hex values for all key codes

--- SP/code/client/cl_keys.c
+++ SP/code/client/cl_keys.c
@@ -1789,6 +1789,7 @@ to be configured even if they don't have defined names.
 */
 int Key_StringToKeynum( char *str ) {
 	keyname_t   *kn;
+	int			n;
 
 	if ( !str || !str[0] ) {
 		return -1;
@@ -1798,12 +1799,9 @@ int Key_StringToKeynum( char *str ) {
 	}
 
 	// check for hex code
-	if ( strlen( str ) == 4 ) {
-		int n = Com_HexStrToInt( str );
-
-		if ( n >= 0 ) {
-			return n;
-		}
+	n = Com_HexStrToInt( str );
+	if ( n >= 0 && n < MAX_KEYS ) {
+		return n;
 	}
 
 	// scan for a text match
