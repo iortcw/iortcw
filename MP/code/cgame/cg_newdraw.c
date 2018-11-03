d6e0fac9c25bba3584ce272ecfb5391f2b5504c8
MAN-AT-ARMS <M4N4T4RMS@gmail.com>
2018-11-03 08:18:25 -0400

    All: Make prevTeamMember command loop around player list

--- MP/code/cgame/cg_newdraw.c
+++ MP/code/cgame/cg_newdraw.c
@@ -149,7 +149,7 @@ int CG_GetSelectedPlayer( void ) {
 
 void CG_SelectNextPlayer( void ) {
 	CG_CheckOrderPending();
-	if ( cg_currentSelectedPlayer.integer >= 0 && cg_currentSelectedPlayer.integer < numSortedTeamPlayers ) {
+	if ( cg_currentSelectedPlayer.integer >= 0 && cg_currentSelectedPlayer.integer <= numSortedTeamPlayers ) {
 		cg_currentSelectedPlayer.integer++;
 	} else {
 		cg_currentSelectedPlayer.integer = 0;
