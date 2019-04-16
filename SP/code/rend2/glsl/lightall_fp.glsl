a00569314e461894609237be04a53d999fdb316d
MAN-AT-ARMS <M4N4T4RMS@gmail.com>
2019-04-16 15:52:51 -0400

    All: Rend2: Disable r_cubeMapping if not OpenGL 3.0+

--- SP/code/rend2/glsl/lightall_fp.glsl
+++ SP/code/rend2/glsl/lightall_fp.glsl
@@ -222,7 +222,7 @@ float CalcLightAttenuation(float point, float normDist)
 	return attenuation;
 }
 
-#if defined(USE_BOX_CUBEMAP_PARALLAX) && __VERSION__ >= 130
+#if defined(USE_BOX_CUBEMAP_PARALLAX)
 vec4 hitCube(vec3 ray, vec3 pos, vec3 invSize, float lod, samplerCube tex)
 {
 	// find any hits on cubemap faces facing the camera
@@ -441,7 +441,7 @@ void main()
 	// from http://seblagarde.wordpress.com/2012/09/29/image-based-lighting-approaches-and-parallax-corrected-cubemap/
 	vec3 parallax = u_CubeMapInfo.xyz + u_CubeMapInfo.w * viewDir;
 
-  #if defined(USE_BOX_CUBEMAP_PARALLAX) && __VERSION__ >= 130
+  #if defined(USE_BOX_CUBEMAP_PARALLAX)
 	vec3 cubeLightColor = hitCube(R * u_CubeMapInfo.w, parallax, u_CubeMapInfo.www, ROUGHNESS_MIPS * roughness, u_CubeMap).rgb * u_EnableTextures.w;
   #else
 	vec3 cubeLightColor = textureCubeLod(u_CubeMap, R + parallax, ROUGHNESS_MIPS * roughness).rgb * u_EnableTextures.w;
