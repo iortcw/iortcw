7449f99664592494cc8f68032ad46518e873507e
MAN-AT-ARMS <M4N4T4RMS@gmail.com>
2019-06-27 16:58:45 -0400

    All: Remove erroneous newlines from Com_Error in sdl_glimp.c

--- SP/code/sdl/sdl_glimp.c
+++ SP/code/sdl/sdl_glimp.c
@@ -341,7 +341,7 @@ static qboolean GLimp_GetProcAddresses( qboolean fixedFunction ) {
 	version = (const char *)qglGetString( GL_VERSION );
 
 	if ( !version ) {
-		Com_Error( ERR_FATAL, "GL_VERSION is NULL\n" );
+		Com_Error( ERR_FATAL, "GL_VERSION is NULL" );
 	}
 
 	if ( Q_stricmpn( "OpenGL ES", version, 9 ) == 0 ) {
@@ -382,10 +382,10 @@ static qboolean GLimp_GetProcAddresses( qboolean fixedFunction ) {
 			qglFogi = GLimp_GLES_Fogi; /*Added*/
 #else
 			// error so this doesn't segfault due to NULL desktop GL functions being used
-			Com_Error( ERR_FATAL, "Unsupported OpenGL Version: %s\n", version );
+			Com_Error( ERR_FATAL, "Unsupported OpenGL Version: %s", version );
 #endif
 		} else {
-			Com_Error( ERR_FATAL, "Unsupported OpenGL Version (%s), OpenGL 1.2 is required\n", version );
+			Com_Error( ERR_FATAL, "Unsupported OpenGL Version (%s), OpenGL 1.2 is required", version );
 		}
 	} else {
 		if ( QGL_VERSION_ATLEAST( 2, 0 ) ) {
@@ -401,9 +401,9 @@ static qboolean GLimp_GetProcAddresses( qboolean fixedFunction ) {
 			QGL_1_5_PROCS;
 			QGL_2_0_PROCS;
 			// error so this doesn't segfault due to NULL desktop GL functions being used
-			Com_Error( ERR_FATAL, "Unsupported OpenGL Version: %s\n", version );
+			Com_Error( ERR_FATAL, "Unsupported OpenGL Version: %s", version );
 		} else {
-			Com_Error( ERR_FATAL, "Unsupported OpenGL Version (%s), OpenGL 2.0 is required\n", version );
+			Com_Error( ERR_FATAL, "Unsupported OpenGL Version (%s), OpenGL 2.0 is required", version );
 		}
 	}
 
