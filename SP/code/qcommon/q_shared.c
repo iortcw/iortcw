efc4eccbb5a5a70c0f340aa510f55a901106c6d0
M4N4T4RMS <M4N4T4RMS@gmail.com>
2020-12-28 07:19:49 -0500

    All: Fix parsing weapon config

--- SP/code/qcommon/q_shared.c
+++ SP/code/qcommon/q_shared.c
@@ -168,7 +168,7 @@ COM_StripFilename
 */
 void COM_StripFilename( char *in, char *out ) {
 	char *end;
-	Q_strncpyz( out, in, sizeof( in ) );
+	memcpy( out, in, strlen( in ) );
 	end = COM_SkipPath( out );
 	*end = 0;
 }
