071f2e8c61945eaeff3aebbd6e3ac8b0a384210a
MAN-AT-ARMS <M4N4T4RMS@gmail.com>
2022-03-29 17:54:47 -0400

    All: Revert 9cf3f73

--- SP/code/cgame/cg_view.c
+++ SP/code/cgame/cg_view.c
@@ -1544,8 +1544,7 @@ void CG_DrawActiveFrame( int serverTime, stereoFrame_t stereoView, qboolean demo
 	DEBUGTIME
 
 	// decide on third person view
-	cg.renderingThirdPerson = cg.snap->ps.persistant[PERS_TEAM] != TEAM_SPECTATOR
-							&& ( cg_thirdPerson.integer || ( cg.snap->ps.stats[STAT_HEALTH] <= 0 ) );
+	cg.renderingThirdPerson = cg_thirdPerson.integer /*|| (cg.snap->ps.stats[STAT_HEALTH] <= 0)*/;
 
 	// build cg.refdef
 	inwater = CG_CalcViewValues();
